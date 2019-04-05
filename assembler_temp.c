facbfb3faec808294e9ba0a0f13b31fec3b7a6cd
NaHyKim <koloke1712@gmail.com>
2019-04-05 21:30:11 +0900

    Initial commit

---
+++ assembler_temp.c
@@ -0,0 +1,97 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+
+/*******************************************************
+ * Function Declaration
+ *
+ *******************************************************/
+char *change_file_ext(char *str);
+
+/*******************************************************
+ * Function: main
+ *
+ * Parameters:
+ *  int
+ *      argc: the number of argument
+ *  char*
+ *      argv[]: array of a sting argument
+ *
+ * Return:
+ *  return success exit value
+ *
+ * Info:
+ *  The typical main function in C language.
+ *  It reads system arguments from terminal (or commands)
+ *  and parse an assembly file(*.s).
+ *  Then, it converts a certain instruction into
+ *  object code which is basically binary code.
+ *
+ *******************************************************/
+int
+main(int argc, char *argv[])
+{
+    FILE *input, *output;
+    char *filename;
+
+    if (argc != 2) {
+        fprintf(stderr, "Usage: %s <*.s>\n", argv[0]);
+        fprintf(stderr, "Example: %s sample_input/example?.s\n", argv[0]);
+        exit(EXIT_FAILURE);
+    }
+
+    input = fopen(argv[1], "r");
+    if (input == NULL) {
+        perror("ERROR");
+        exit(EXIT_FAILURE);
+    }
+
+    filename = strdup(argv[1]); // strdup() is not a standard C library but fairy used a lot.
+    if(change_file_ext(filename) == NULL) {
+        fprintf(stderr, "'%s' file is not an assembly file.\n", filename);
+        exit(EXIT_FAILURE);
+    }
+
+    output = fopen(filename, "w");
+    if (output == NULL) {
+        perror("ERROR");
+        exit(EXIT_FAILURE);
+    }
+
+    // process();
+
+    fclose(input);
+    fclose(output);
+    exit(EXIT_SUCCESS);
+}
+
+
+/*******************************************************
+ * Function: change_file_ext
+ *
+ * Parameters:
+ *  char
+ *      *str: a raw filename (without path)
+ *
+ * Return:
+ *  return NULL if a file is not an assembly file.
+ *  return empty string
+ *
+ * Info:
+ *  This function reads filename and converst it into
+ *  object extention name, *.o
+ *
+ *******************************************************/
+char
+*change_file_ext(char *str)
+{
+    char *dot = strrchr(str, '.');
+
+    if (!dot || dot == str || (strcmp(dot, ".s") != 0)) {
+        return NULL;
+    }
+
+    str[strlen(str) - 1] = 'o';
+    return "";
+}
